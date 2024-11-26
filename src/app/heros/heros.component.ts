a8821ce04a60136663faac93d2c36cb90396f4c3
gunateja.mekathoti <gunateja.mekathoti@accenture.com>
2024-11-26 23:45:13 +0530

    added ngmodel

---
+++ src/app/heros/heros.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { hero } from '../hero';
+@Component({
+  selector: 'app-heros',
+  standalone: false,
+  
+  templateUrl: './heros.component.html',
+  styleUrl: './heros.component.css'
+})
+export class HerosComponent {
+ Hero: hero= {
+  id :1,
+  name :"guna"
+
+ }
+}
